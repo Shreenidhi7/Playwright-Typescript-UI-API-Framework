4ef7c08d762c1219f92eebf3d40b3d539ae727c8
Shreenidhi7 <shreenidhisharma7@gmail.com>
2026-08-16 09:25:17 +0530

    api test = product spec

---
+++ tests/api/products.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+import { RequestHandler } from "../../utils/requestHandler";
+
+let baseUrl = "https://api.practicesoftwaretesting.com/"
+test("Get Products", async ({ request }) => {
+    const req = new RequestHandler(request, baseUrl)
+    const response = await req.path("products").params({
+        "page": 1,
+        "between": "price, 1, 100",
+        "is_rental": false
+    }).getRequest()
+
+    const jsonResponse = await response.json()
+    console.log("Values present at index 0", jsonResponse.data[0]);
+    expect(jsonResponse.data[0].id).toEqual("01M0486KER086MA3ZPBAM081WS")
+    expect(jsonResponse.data[0].price).toEqual(14.15)
+    expect(jsonResponse.data[0].id).toEqual(expect.any(String))
+    expect(jsonResponse.data[0].product_image.by_url).toEqual("https://unsplash.com/@fantin")
+})
---
+++ utils/requestHandler.ts
@@ -0,0 +1,64 @@
+import { APIRequestContext, test } from "@playwright/test";
+import { Url } from "node:url";
+
+export class RequestHandler {
+    private request: APIRequestContext
+    private baseUrl: string | undefined
+    private apiBaseUrl: string
+    private apiPath: string = ""
+    private queryParams: object = {}
+    private apiHeaders: Record<string, string> = {}
+    private apiBody: object = {}
+
+    constructor(request: APIRequestContext, apiBaseUrl: string) {
+        this.request = request
+        this.apiBaseUrl = apiBaseUrl
+    }
+
+    //Builder Pattern (Fluent Pattern)
+    /*
+    * const request = new RequestHandler("dummy request", "dummy url")
+    * request.url("url")
+    * request.path("path")
+    * request.url("url").path("path") => Method Chainig
+    */
+    //setter method
+    url(baseUrl: string) {
+        this.baseUrl = baseUrl
+        return this
+    }
+
+    path(apiPath: string) {
+        this.apiPath = apiPath
+        return this
+    }
+
+    params(queryParams: object) {
+        this.queryParams = queryParams
+        return this
+    }
+
+    headers(apiHeaders: Record<string, string>) {
+        this.apiHeaders = apiHeaders
+        return this
+    }
+
+    body(apiBody: object) {
+        this.apiBody = apiBody
+        return this
+    }
+
+    async getRequest() {
+        let response: any
+        const fullUrl = `${this.apiBaseUrl}${this.apiPath}`
+        await test.step(`Get Request to: ${fullUrl}`, async () => {
+            response = await this.request.get(fullUrl, {
+                headers: this.apiHeaders,
+                params: this.queryParams as {
+                    [key: string]: string | number | boolean
+                }
+            })
+        })
+        return response
+    }
+}
